e6f81eb445c2f4c208224e93f2208773aa5c1aa0
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-08-04 14:00:31 -0500

    add form and validate form

---
+++ index.js
@@ -0,0 +1,40 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const $form = document.querySelector("#form-ux");
+  const $text = document.querySelector("#text");
+
+  $form[0].form[0].focus();
+
+  $form.addEventListener("keypress", (e) => {
+    if (e.code == "Enter") {
+      if ($form.nombres.value !== "") {
+        $form.documento.focus();
+      }
+      if ($form.documento.value !== "") {
+        $form.correo.focus();
+      }
+      if ($form.correo.value !== "") {
+        $form.mensaje.focus();
+      }
+      if ($form.mensaje.value !== "") {
+        $form.submit.focus();
+      }
+      $form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        if (
+          $form.nombres.value !== "" &&
+          $form.documento.value !== "" &&
+          $form.correo.value !== "" &&
+          $form.mensaje.value !== ""
+        ) {
+          $text.innerText = "Completo";
+          setTimeout(() => {
+            $text.innerText = "";
+            $form.reset();
+            $form.nombres.focus();
+          }, 2000);
+        }
+      });
+    }
+  });
+});
